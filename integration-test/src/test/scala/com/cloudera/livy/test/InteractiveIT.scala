aa21f71b886b4c4bf8a6f5b2f8e78092b7c4f38b
Alex Man <tc.technetium@gmail.com>
2016-09-11 18:48:41 +0800

    Implemented interactive statement results recovery.
    - Removed statement results history cache in livy-server.
    - Proxy statement results history requests to livy-repl.
    - Modified RSC repl API:
     - ReplJobRequest to be synchronus and return the id of new statement.
     - GetReplJobResults to return all or some statements in sorted order. Also return current repl state.
     - Moved definition of Statement and StatementState to repl-rsc to avoid unnecessary parsing.
    - livy-server polls only repl state but not statement results from repl.
    - New StatementState "waiting" to show a stetement that's submitted to repl but hasn't been executed.

--- integration-test/src/test/scala/com/cloudera/livy/test/InteractiveIT.scala
+++ integration-test/src/test/scala/com/cloudera/livy/test/InteractiveIT.scala
@@ -127,7 +127,8 @@ class InteractiveIT extends BaseIntegrationTestSuite {
 
   test("recover interactive session") {
     withNewSession(Spark()) { s =>
-      s.run("1").verifyResult("res0: Int = 1")
+      val stmt1 = s.run("1")
+      stmt1.verifyResult("res0: Int = 1")
 
       // Restart Livy.
       cluster.stopLivy()
@@ -136,7 +137,8 @@ class InteractiveIT extends BaseIntegrationTestSuite {
       // Verify session still exists.
       s.verifySessionIdle()
       s.run("2").verifyResult("res1: Int = 2")
-      // TODO, verify previous statement results still exist.
+      // Verify statement result is preserved.
+      stmt1.verifyResult("res0: Int = 1")
 
       s.stop()
 
